65be5cc454ffceb87850265708358f709b3bf71c
Enzo Seehagen <enzovonseehagen@gmail.com>
2023-05-14 17:12:25 -0300

    Refatorando o código aplicando design pattern Strategy

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -9,16 +9,10 @@ public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
 
-        if (desempenho == Desempenho.A_DESEJAR) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+            BigDecimal percentual = desempenho.percentualReajuste();
+            BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
             funcionario.reajustarSalario(reajuste);
-        } else if (desempenho == Desempenho.BOM) {
-            BigDecimal reajuste2 = funcionario.getSalario().multiply(new BigDecimal("0.10"));
-            funcionario.reajustarSalario(reajuste2);
-        } else { 
-            BigDecimal reajuste3 = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.reajustarSalario(reajuste3);
-        } 
+      
     }
 
 }
